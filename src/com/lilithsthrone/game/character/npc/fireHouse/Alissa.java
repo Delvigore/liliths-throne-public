6c6713ca11ee92d0eab077e4c77a3e732896ac4a
Delvigore <Delvigore@192.168.0.1>
2021-02-18 18:48:04 -0700

    New NPC
    Attempt to create new NPC.  She's an otter-morph but she registers in
    game as a dog morph and I don't know how to fix it

---
+++ src/com/lilithsthrone/game/character/npc/fireHouse/Alissa.java
@@ -0,0 +1,265 @@
+package com.lilithsthrone.game.character.npc.fireHouse;
+
+import java.time.Month;
+import java.util.List;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import com.lilithsthrone.game.character.CharacterImportSetting;
+import com.lilithsthrone.game.character.EquipClothingSetting;
+import com.lilithsthrone.game.character.GameCharacter;
+import com.lilithsthrone.game.character.attributes.Attribute;
+import com.lilithsthrone.game.character.body.coverings.BodyCoveringType;
+import com.lilithsthrone.game.character.body.coverings.Covering;
+import com.lilithsthrone.game.character.body.valueEnums.AreolaeSize;
+import com.lilithsthrone.game.character.body.valueEnums.AssSize;
+import com.lilithsthrone.game.character.body.valueEnums.BodyHair;
+import com.lilithsthrone.game.character.body.valueEnums.BreastShape;
+import com.lilithsthrone.game.character.body.valueEnums.Capacity;
+import com.lilithsthrone.game.character.body.valueEnums.ClitorisSize;
+import com.lilithsthrone.game.character.body.valueEnums.CupSize;
+import com.lilithsthrone.game.character.body.valueEnums.HairLength;
+import com.lilithsthrone.game.character.body.valueEnums.HairStyle;
+import com.lilithsthrone.game.character.body.valueEnums.HipSize;
+import com.lilithsthrone.game.character.body.valueEnums.LabiaSize;
+import com.lilithsthrone.game.character.body.valueEnums.LipSize;
+import com.lilithsthrone.game.character.body.valueEnums.NippleSize;
+import com.lilithsthrone.game.character.body.valueEnums.OrificeElasticity;
+import com.lilithsthrone.game.character.body.valueEnums.OrificePlasticity;
+import com.lilithsthrone.game.character.body.valueEnums.TongueLength;
+import com.lilithsthrone.game.character.body.valueEnums.Wetness;
+import com.lilithsthrone.game.character.effects.Perk;
+import com.lilithsthrone.game.character.effects.PerkCategory;
+import com.lilithsthrone.game.character.effects.PerkManager;
+import com.lilithsthrone.game.character.fetishes.Fetish;
+import com.lilithsthrone.game.character.fetishes.FetishDesire;
+import com.lilithsthrone.game.character.gender.Gender;
+import com.lilithsthrone.game.character.npc.NPC;
+import com.lilithsthrone.game.character.persona.NameTriplet;
+import com.lilithsthrone.game.character.persona.Occupation;
+import com.lilithsthrone.game.character.persona.PersonalityTrait;
+import com.lilithsthrone.game.character.persona.SexualOrientation;
+import com.lilithsthrone.game.character.race.RaceStage;
+import com.lilithsthrone.game.character.race.Subspecies;
+import com.lilithsthrone.game.dialogue.DialogueNode;
+import com.lilithsthrone.game.inventory.CharacterInventory;
+import com.lilithsthrone.game.inventory.clothing.ClothingType;
+import com.lilithsthrone.game.sex.SexPace;
+import com.lilithsthrone.main.Main;
+import com.lilithsthrone.utils.Util;
+import com.lilithsthrone.utils.Util.Value;
+import com.lilithsthrone.utils.colours.PresetColour;
+import com.lilithsthrone.world.WorldType;
+import com.lilithsthrone.world.places.PlaceType;
+
+/**
+ * @author Delvigore, with much assistance from Innoxia and deboucher 
+ */
+
+public class Alissa extends NPC {
+	
+	public Alissa() {
+		this(false);
+	}
+	
+	public Alissa(boolean isImported) {
+		super(isImported, new NameTriplet("Alissa"), "Carson",
+				"A person stationed at the fancy firehouse"
+						+ " She's a secretary here.",
+				24, Month.AUGUST, 11,
+				15,
+				Gender.F_V_B_FEMALE,
+				Subspecies.getSubspeciesFromId("dsg_otter"),
+				RaceStage.GREATER,
+				new CharacterInventory(10), WorldType.DOMINION_FIREHOUSE, PlaceType.FIREHOUSE_SECRETARY, true);
+		
+		if(!isImported) {
+			this.setPlayerKnowsName(false);
+			this.setGenericName("Otter girl");
+		}
+	}
+	
+
+	@Override
+	public void setupPerks(boolean autoSelectPerks) {
+		this.addSpecialPerk(Perk.SPECIAL_SLUT);
+		
+		PerkManager.initialisePerks(this,
+				Util.newArrayListOfValues(
+						Perk.RUNNER_2,
+						Perk.SEDUCTION_DEFENCE_BOOST,
+						Perk.OBSERVANT,
+						Perk.ENCHANTMENT_STABILITY,
+						Perk.COMBAT_REGENERATION),
+				Util.newHashMapOfValues(
+						new Value<>(PerkCategory.PHYSICAL, 5),
+						new Value<>(PerkCategory.LUST, 2),
+						new Value<>(PerkCategory.ARCANE, 0)));
+	}
+
+	@Override
+	public void resetDefaultMoves() {
+		this.clearEquippedMoves();
+		equipMove("strike");
+		equipMove("offhand-strike");
+		equipMove("twin-strike");
+		equipMove("block");
+		this.equipAllSpellMoves();
+	}
+	
+	@Override
+	public void setStartingBody(boolean setPersona) {
+		// Persona:
+		if(setPersona) {
+			this.setPersonalityTraits(
+					PersonalityTrait.CONFIDENT,
+					PersonalityTrait.KIND);
+	
+			this.setSexualOrientation(SexualOrientation.AMBIPHILIC);
+			
+			this.setHistory(Occupation.NPC_PROSTITUTE);
+
+			this.addFetish(Fetish.FETISH_EXHIBITIONIST);
+			this.addFetish(Fetish.FETISH_ORAL_GIVING);
+			this.addFetish(Fetish.FETISH_CUM_ADDICT);
+			this.setFetishDesire(Fetish.FETISH_VAGINAL_RECEIVING, FetishDesire.FOUR_LOVE);
+			this.setFetishDesire(Fetish.FETISH_DOMINANT, FetishDesire.THREE_LIKE);
+			this.setFetishDesire(Fetish.FETISH_PURE_VIRGIN, FetishDesire.ONE_DISLIKE);
+			this.setFetishDesire(Fetish.FETISH_SADIST, FetishDesire.ZERO_HATE);
+		}
+		
+		// Body:
+
+		// Core:
+		this.setHeight(160);
+		this.setFemininity(70);
+		this.setMuscle(50);
+		this.setBodySize(50);
+		this.setAttribute(Attribute.MAJOR_CORRUPTION, 15);
+		
+		// Coverings:
+		this.setEyeCovering(new Covering(BodyCoveringType.getBodyCoveringTypeFromId("dsg_otter_eye"), PresetColour.EYE_BROWN));
+		this.setSkinCovering(new Covering(BodyCoveringType.getBodyCoveringTypeFromId("dsg_otter_fur"), PresetColour.COVERING_TAN), true);
+		this.setSkinCovering(new Covering(BodyCoveringType.HUMAN, PresetColour.SKIN_LIGHT), true);
+
+		this.setHairCovering(new Covering(BodyCoveringType.getBodyCoveringTypeFromId("dsg_otter_hair"), PresetColour.COVERING_BLACK), true);
+		this.setHairLength(HairLength.THREE_SHOULDER_LENGTH.getMedianValue());
+		this.setHairStyle(HairStyle.LOOSE);
+
+		this.setHairCovering(new Covering(BodyCoveringType.getBodyCoveringTypeFromId("dsg_otter_body_hair"), PresetColour.COVERING_BROWN_DARK), false);
+		this.setUnderarmHair(BodyHair.ZERO_NONE);
+		this.setAssHair(BodyHair.ZERO_NONE);
+		this.setPubicHair(BodyHair.ZERO_NONE);
+		this.setFacialHair(BodyHair.ZERO_NONE);
+
+		// Face:
+		this.setFaceVirgin(false);
+		this.setLipSize(LipSize.TWO_FULL);
+		this.setFaceCapacity(Capacity.FIVE_ROOMY, true);
+		// Throat settings and modifiers
+		this.setTongueLength(TongueLength.ONE_LONG.getMedianValue());
+		// Tongue modifiers
+		
+		// Chest:
+		this.setBreastRows(1);
+		this.setNippleVirgin(true);
+		this.setBreastSize(CupSize.D.getMeasurement());
+		this.setBreastShape(BreastShape.PERKY);
+		this.setNippleSize(NippleSize.TWO_BIG.getValue());
+		this.setAreolaeSize(AreolaeSize.TWO_BIG.getValue());
+		// Nipple settings and modifiers
+		
+		// Ass:
+		this.setAssVirgin(true);
+		this.setAssBleached(false);
+		this.setAssSize(AssSize.THREE_NORMAL);
+		this.setHipSize(HipSize.THREE_GIRLY);
+		this.setAssCapacity(Capacity.TWO_TIGHT, true);
+		this.setAssWetness(Wetness.ZERO_DRY);
+		this.setAssElasticity(OrificeElasticity.FOUR_LIMBER.getValue());
+		this.setAssPlasticity(OrificePlasticity.THREE_RESILIENT.getValue());
+		// Anus modifiers
+		
+		// Penis:
+		// No penis
+		
+		// Vagina:
+		this.setVaginaVirgin(false);
+		this.setVaginaClitorisSize(ClitorisSize.ZERO_AVERAGE);
+		this.setVaginaLabiaSize(LabiaSize.ZERO_TINY);
+		this.setVaginaSquirter(true);
+		this.setVaginaCapacity(Capacity.THREE_SLIGHTLY_LOOSE, true);
+		this.setVaginaDepth(3);
+		this.setVaginaWetness(Wetness.FOUR_SLIMY);
+		this.setVaginaElasticity(OrificeElasticity.FOUR_LIMBER.getValue());
+		this.setVaginaPlasticity(OrificePlasticity.THREE_RESILIENT.getValue());
+	}
+	
+	@Override
+	public void equipClothing(List<EquipClothingSetting> settings) {
+		this.unequipAllClothingIntoVoid(true, true);
+		this.clearNonEquippedInventory(false);
+		this.unequipMainWeaponIntoVoid(0, false);
+		this.unequipOffhandWeaponIntoVoid(0, false);
+		
+	//	this.equipClothingFromNowhere(Main.game.getItemGen().generateClothing(ClothingType.CHEST_TUBE_TOP, PresetColour.CLOTHING_BLUE_DARK, false), true, this);
+	//	this.equipClothingFromNowhere(Main.game.getItemGen().generateClothing(ClothingType.GROIN_BOYSHORTS, PresetColour.CLOTHING_BLACK, false), true, this);
+	//	this.equipClothingFromNowhere(Main.game.getItemGen().generateClothing("nerdlinger_street_joggers_joggers", PresetColour.CLOTHING_GREY, false), true, this);
+	//	this.equipClothingFromNowhere(Main.game.getItemGen().generateClothing("innoxia_sock_socks", PresetColour.CLOTHING_WHITE, false), true, this);
+	//	this.equipClothingFromNowhere(Main.game.getItemGen().generateClothing("innoxia_foot_low_top_skater_shoes", PresetColour.CLOTHING_BLACK, false), true, this);
+				
+		
+		if(settings.contains(EquipClothingSetting.ADD_ACCESSORIES)) {
+			this.equipClothingFromNowhere(Main.game.getItemGen().generateClothing(ClothingType.WRIST_WOMENS_WATCH, PresetColour.CLOTHING_BLACK, false), true, this);
+		}
+		
+	}
+	
+	@Override
+	public boolean isUnique() {
+		return true;
+	}
+	
+	@Override
+	public boolean isAbleToBeImpregnated() {
+		return true;
+	}
+	
+	@Override
+	public void changeFurryLevel(){
+	}
+
+	@Override
+	public DialogueNode getEncounterDialogue() {
+		return null;
+	}
+
+	@Override
+	public void turnUpdate() {
+		if(!Main.game.getCharactersPresent().contains(this)) {
+			if(Main.game.isFireDayShift()) {
+				this.setLocation(WorldType.DOMINION_FIREHOUSE, PlaceType.FIREHOUSE_SECRETARY, true);
+				
+			} else {
+				this.setLocation(WorldType.DOMINION_FIREHOUSE2, PlaceType.FIREHOUSE_FLOOR1, false);
+			}
+		}
+	}
+		
+	@Override
+	public SexPace getSexPaceSubPreference(GameCharacter character){
+		return SexPace.SUB_NORMAL;
+	}
+
+	@Override
+	public SexPace getSexPaceDomPreference(){
+		return SexPace.DOM_NORMAL;
+	}
+	
+	@Override
+	public void loadFromXML(Element parentElement, Document doc, CharacterImportSetting... settings) {
+	    loadNPCVariablesFromXML(this, null, parentElement, doc, settings);
+	}
+	
+}
